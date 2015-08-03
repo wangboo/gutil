010c555627c98e61fca98983615f2863424e1b83
wangboo <advance345@126.com>
2015-08-03 16:51:15 +0800

    add mgo session_inject

--- revelmgo/session_inject.go
+++ revelmgo/session_inject.go
@@ -6,6 +6,7 @@ import (
 	"reflect"
 )
 
+// 创建revel_session 注入过滤器
 func MgoSessionInjectFilterFunc(session *mgo.Session) func(c *revel.Controller, fc []revel.Filter) {
 	return func(c *revel.Controller, fc []revel.Filter) {
 		appCtrl := c.AppController
@@ -24,4 +25,17 @@ func MgoSessionInjectFilterFunc(session *mgo.Session) func(c *revel.Controller, 
 		valoeOfElem.Set(valueOfSession)
 		fc[0](c, fc[1:])
 	}
+}
+
+// 重新生成带revel_session 注入过滤器的Filters
+func NewFiltersWithMgoInjecter(filters []revel.Filter, session *mgo.Session) []revel.Filter {
+	newFilters := []revel.Filter{}
+	typeOfActionInvoker := reflect.TypeOf(revel.ActionInvoker)
+	for _, o := range filters {
+		if reflect.TypeOf(o) == typeOfActionInvoker {
+			newFilters = append(newFilters, MgoSessionInjectFilterFunc(session))
+		}
+		newFilters = append(newFilters, o)
+	}
+	return newFilters
 }
